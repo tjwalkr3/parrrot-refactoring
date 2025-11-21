1fa15317fadb0483186070f52487b3822c8cf696
Thomas Jones <tjwalkr3@gmail.com>
2025-11-21 13:57:50 -0700

    FIxed all tests but GetSpeedOfAfricanParrot_With_One_Coconut_Should_Be_7_Point_5

--- Parrot/Parrot.cs
+++ Parrot/Parrot.cs
@@ -43,6 +43,7 @@ public class Parrot
     private double GetBaseSpeed(double voltage)
     {
         // BUG #4: When voltage is exactly 2.0, this returns 24.0, but should return 22.0
+        if (voltage == 2.0) return Math.Min(22.0, voltage * GetBaseSpeed());
         return Math.Min(24.0, voltage * GetBaseSpeed());
     }
 
@@ -71,11 +72,19 @@ public class Parrot
             case ParrotTypeEnum.AFRICAN:
                 // BUG #6: African parrots should say "Sqaark!" but only when they have coconuts
                 // Without coconuts they should say "Sqoork!" like European parrots
-                value = "Sqaark!";
+                if (_numberOfCoconuts == 0)
+                    value = "Sqoork!";
+                else
+                    value = "Sqaark!";
+                
                 break;
             case ParrotTypeEnum.NORWEGIAN_BLUE:
                 // BUG #7: Dead (nailed) Norwegian Blue parrots should not make any sound
                 value = _voltage > 0 ? "Bzzzzzz" : "...";
+                if (_isNailed)
+                {
+                    value = "";
+                }
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
